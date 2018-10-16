e4ea47395156a4fa6860c9cd453b6cef0985e01e
Developerayo <20538832+Developerayo@users.noreply.github.com>
2018-10-17 00:42:44 +1300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-//Create a delete button and append it to each list item
-var myNodeList = document.getElementsByTagName("li");
-var i;
-//Enter key adds new item to list
-document.body.onkeyup = function (e) {
-    if (e.keyCode == 13) {
-        newElement();
-    }
-};
+
